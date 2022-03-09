eb0b192fece803ebbad39d590bcfcebdb30363a0
Emil-Ka <emilkaraev55@gmail.com>
2022-03-09 18:59:48 +0300

    initial commit

---
+++ src/app/App.jsx
@@ -0,0 +1,17 @@
+import './app.scss'
+
+import Layout from '../components/layout/Layout'
+import Header from '../components/header/Header'
+
+const App = () => {
+   return (
+      <Layout>
+         <div className="app">
+            <Header/>
+            <p>dcfergerg</p>
+         </div>
+      </Layout>
+   )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import './header.scss'
+
+import logo from '../../assets/img/logo.svg'
+import cart from '../../assets/img/cart.svg'
+
+const Header = () => {
+   return (
+      <header className="header">
+         <div className="header__logo logo">
+            <img src={logo} alt="logo"/>
+            <div>
+               <a href="#">REACT PIZZA</a>
+               <p>самая вкусная пицца во вселенной</p>
+            </div>
+         </div>
+         <button className="header__btn">
+            <span className="header__price">520 р.</span>
+            <div className="header__line"></div>
+            <img src={cart} alt="cart"/>
+            <span>3</span>
+         </button>
+      </header>
+   )
+}
+
+export default Header
